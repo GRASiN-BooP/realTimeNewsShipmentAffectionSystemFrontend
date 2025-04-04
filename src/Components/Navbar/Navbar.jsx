0422617bc93459122cf8ec959dffadfe77200299
GRASiN-BooP <gaurav.singh@gocomet.com>
2025-04-04 15:04:11 +0530

    added routing in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const user = false;
   return (
     <nav className="w-full flex justify-between items-center p-4 h-16 fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
-      <img src="/GoCometLogoWithTitle.png" alt="Brandname" className="w-36" />
+      <Link to="/">
+        <img src="/GoCometLogoWithTitle.png" alt="Brandname" className="w-36" />
+      </Link>
       {user && (
         <div>
           <button className="px-4 py-2 rounded-md text-white font-gocomet bg-linear-to-r from-blue-900 to-gocomet">
