46ee0536f83efdfdb85fd1104b0ab632b9d1eaa5
GRASiN-BooP <gaurav.singh@gocomet.com>
2025-04-06 18:04:11 +0530

    made some style changes

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useSpring } from "motion/react";
 
 export default function Card(props) {
+  const bgcolor =
+    props.state === "danger"
+      ? "bg-red-500/25"
+      : props.state === "caution"
+      ? "bg-yellow-500/25"
+      : "bg-blue-500/25";
   const color =
     props.state === "danger"
-      ? "bg-red-500"
+      ? "text-red-500"
       : props.state === "caution"
-      ? "bg-yellow-500"
-      : "bg-blue-500";
+      ? "text-yellow-500"
+      : "text-blue-500";
   const [displayCount, setDisplayCount] = useState(0);
   const springCount = useSpring(0, {
     bounce: 0,
@@ -22,7 +28,7 @@ export default function Card(props) {
   return (
     <div className="bg-white min-h-16 h-full rounded-lg p-2 flex flex-col items-center justify-center text-center gap-2 shadow-md">
       <div
-        className={`${color} w-20 h-20 p-2 flex items-center justify-center rounded-full  text-white text-5xl font-bold`}
+        className={`${bgcolor} w-20 h-20 p-2 flex items-center justify-center rounded-lg  ${color} text-5xl font-bold`}
       >
         {displayCount}
       </div>
